00b4d1845348cfc456fb7ac107efc173dc948edb
George Bastos <114554305+GeorgeBastosFerreiraFH@users.noreply.github.com>
2025-03-17 15:53:21 -0300

    Create produtos.py

---
+++ produtos.py
@@ -0,0 +1,8 @@
+class Produto():
+    def __init__(self, nome, descricao, qntd_estoque):
+        self.nome = nome
+        self.descricao = descricao
+        self.qntd_estoque = qntd_estoque
+
+    def verProdutos():
+        f'Nome: {self.nome}'
